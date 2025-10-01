3403521a49f4e6274143023d0f2e1e90cf5c61cb
AdminWeb09 <arsenioandyzg@gmail.com>
2025-10-01 13:33:11 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-import React, { useState, useEffect } from "react";
-
 const GirlfriendDayWebsite = () => {
   const [currentPage, setCurrentPage] = useState(0);
   const [isPlaying, setIsPlaying] = useState(true);
